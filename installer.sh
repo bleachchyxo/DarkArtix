2ee289d0e2acfff79e3021abdd0d2732d5c413d4
bleachchyxo <85224206+bleachchyxo@users.noreply.github.com>
2025-10-21 19:47:13 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -16,15 +16,14 @@ fi
 
 # notification system
 message() {
-    local color="$1"
-    local msg="$2"
-    
-    case "$color" in
-        green) echo -e "\033[32m[+]\033[0m $msg" ;;
-        yellow) echo -e "\033[33m[+]\033[0m $msg" ;;
-        blue) echo -e "\033[34m[+]\033[0m $msg" ;;
-        *) echo "[+] $msg" ;; # Default case (no color for [+])
-    esac
+  local color="$1"
+  local msg="$2"
+  case "$color" in
+    green) echo -e "\033[32m[+]\033[0m $msg" ;;
+    yellow) echo -e "\033[33m[+]\033[0m $msg" ;;
+    blue) echo -e "\033[34m[+]\033[0m $msg" ;;
+    *) echo "[+] $msg" ;;
+  esac
 }
 
 default_prompt() {
@@ -47,5 +46,17 @@ confirmation() {
 echo "DarkArtix Installer v0.1"
 echo "Firmware: $firmware"
 message blue "Choosing a disk"
-lsblk -dno NAME,SIZE,TYPE | awk '$3 == "disk" && $1 !~ /loop/ && $1 !~ /ram/ {print "  " $1, $2}'
+mapfile -t disks < <(lsblk -dno NAME,SIZE,TYPE | awk '$3 == "disk" && $1 !~ /loop/ && $1 !~ /ram/ {print $1, $2}')
+for disk_entry in "${disks[@]}"; do
+  echo "  $disk_entry"
+done
 
+default_disk="${disks[0]%% *}"
+disk_name=$(default_prompt "Choose a disk to install" "$default_disk")
+disk="/dev/$disk_name"
+
+if [[ ! -b "$disk" ]]; then
+  echo "Invalid disk: $disk"
+  exit 1
+fi
+confirmation "This will erase all data on $disk. Continue?" "no"
