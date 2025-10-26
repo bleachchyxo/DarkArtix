04c36db4238fd8c8d01f19df6b93ebff77d6595c
bleachchyxo <85224206+bleachchyxo@users.noreply.github.com>
2025-10-26 19:22:58 +0200

    Update enviroment.sh

--- enviroment.sh
+++ enviroment.sh
@@ -38,3 +38,23 @@ for repository in dwm dmenu st; do
 done
 
 chown -R "$USER:$USER" "$HOME/.config"
+
+# dmenu
+sed -i 's/static int topbar = 1;/static int topbar = 0;/' $HOME/.config/dmenu/config.def.h
+make -C $HOME/.config/dmenu install
+
+# dwm
+curl -s -o $HOME/.config/dwm/dwm-fullgaps-20200508-7b77734.diff https://dwm.suckless.org/patches/fullgaps/dwm-fullgaps-20200508-7b77734.diff
+patch -d $HOME/.config/dwm < $HOME/.config/dwm/dwm-fullgaps-20200508-7b77734.diff
+sed -i 's/static const unsigned int gappx     = 5;/static const unsigned int gappx     = 15;/' $HOME/.config/dwm/config.def.h
+sed -i '8s/topbar\s*=\s*1;/topbar = 0;/g' $HOME/.config/dwm/config.def.h
+sed -i 's/#define MODKEY Mod1Mask/#define MODKEY Mod4Mask/' $HOME/.config/dwm/config.def.h
+make -C $HOME/.config/dwm install
+rm $HOME/.config/dwm/dwm-fullgaps-20200508-7b77734.diff
+rm $HOME/.config/dwm/*.orig
+
+# st
+curl -s -o $HOME/.config/st/st-alpha-20240814-a0274bc.diff https://st.suckless.org/patches/alpha/st-alpha-20240814-a0274bc.diff
+patch -d $HOME/.config/st < $HOME/.config/st/st-alpha-20240814-a0274bc.diff
+curl -s -o $HOME/.config/st/st-blinking_cursor-20230819-3a6d6d7.diff https://st.suckless.org/patches/blinking_cursor/st-blinking_cursor-20230819-3a6d6d7.diff
+patch -d $HOME/.config/st < $HOME/.config/st/st-blinking_cursor-20230819-3a6d6d7.diff
