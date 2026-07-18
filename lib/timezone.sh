24c499f94dc3b6069b603b49281706ed56e3ecb3
bleachchyxo <85224206+bleachchyxo@users.noreply.github.com>
2026-07-18 19:54:01 +0300

    Delete lib directory

--- lib/timezone.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Set timezone and locale
-set_timezone() {
-  timezone=$(ask "Enter your timezone (e.g., Europe/Berlin)" "UTC")
-  ln -sf /usr/share/zoneinfo/$timezone /etc/localtime
-  hwclock --systohc
-  locale-gen
-}
